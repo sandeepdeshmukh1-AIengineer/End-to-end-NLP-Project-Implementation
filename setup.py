e2c6a64e84d6592f94aef30f0a96503a0d964eae
Sandeep Deshmukh <sandeepsdeshmukh1@gmail.com>
2025-04-22 18:42:56 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="sandeep deshmukh",
+    author_email="sandeepsdeshmukh1@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
