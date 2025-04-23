2e806978b7ca672693fac217c62a0e0ef7cc8654
Sandeep Deshmukh <sandeepsdeshmukh1@gmail.com>
2025-04-23 10:41:16 +0530

    new added

--- demo.py
+++ demo.py
@@ -4,4 +4,5 @@ import sys
 from hate.configuration.gcloud_syncer import GCloudSync
 
 obj = GCloudSync()
-obj.sync_folder_from_gcloud("hate-speech2024", "dataset.zip", "download/dataset.zip")
+obj.sync_folder_from_gcloud("hate-speech2025", "dataset.zip", "download/dataset.zip")
+ 
