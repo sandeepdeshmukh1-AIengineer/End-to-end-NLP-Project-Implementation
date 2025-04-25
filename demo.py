a59907a2371ebe0130965bbf13331393a4d484b8
Sandeep Deshmukh <sandeepsdeshmukh1@gmail.com>
2025-04-25 12:41:50 +0530

    data ingestion components added

--- demo.py
+++ demo.py
@@ -1,8 +1,2 @@
-from hate.logger import logging
-from hate.exception import CustomException
-import sys
-from hate.configuration.gcloud_syncer import GCloudSync
 
-obj = GCloudSync()
-obj.sync_folder_from_gcloud("hate-speech2025", "dataset.zip", "download/dataset.zip")
  
